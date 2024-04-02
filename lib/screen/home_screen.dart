00a58deac92fc7ce1806b3db42d4a2b1424cfbf0
Juyeon Kim <kkjuyeon@gmail.com>
2024-04-02 17:49:04 +0900

    feat: app bar 퍼블리싱

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,13 +1,18 @@
+import 'package:dusty_dust/component/main_app_bar.dart';
+import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+      backgroundColor: primaryColor,
+      body: CustomScrollView(
+        slivers: [
+          MainAppBar()
+        ],
       )
     );
   }
