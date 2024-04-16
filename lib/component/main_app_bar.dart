fca68999fb489634fed27a6fafa11e952d706f11
Juyeon Kim <kkjuyeon@gmail.com>
2024-04-16 22:07:30 +0900

    feat: SliverAppBar에 데이터 바인딩

--- lib/component/main_app_bar.dart
+++ lib/component/main_app_bar.dart
@@ -1,55 +1,75 @@
 import 'package:dusty_dust/const/colors.dart';
+import 'package:dusty_dust/model/stat_model.dart';
+import 'package:dusty_dust/model/status_model.dart';
 import 'package:flutter/material.dart';
 
 class MainAppBar extends StatelessWidget {
-  const MainAppBar({super.key});
+  final StatusModel status;
+  final StatModel stat;
+
+  const MainAppBar({super.key, required this.status, required this.stat});
 
   @override
   Widget build(BuildContext context) {
-    final ts = TextStyle( // 자주쓰는 폰트 색상 상수화
+    final ts = TextStyle(
+        // 자주쓰는 폰트 색상 상수화
         color: Colors.white,
-        fontSize: 30.0
-    );
+        fontSize: 30.0);
     return SliverAppBar(
-      backgroundColor: primaryColor,
+      backgroundColor: status.primaryColor,
       expandedHeight: 500,
       // 스크롤 시 사라지게 할 수 있는 공간
       flexibleSpace: FlexibleSpaceBar(
           background: SafeArea(
-            child: Container(
-              margin: EdgeInsets.only(top:kToolbarHeight), // 자동 앱바 간격
-              child: Column(
-                children: [
-                  Text('서울', style: ts.copyWith( // 구조분해할당
+        child: Container(
+          margin: EdgeInsets.only(top: kToolbarHeight), // 자동 앱바 간격
+          child: Column(
+            children: [
+              Text(
+                '서울',
+                style: ts.copyWith(
+                  // 구조분해할당
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Text(
+                getTimeFromDateTime(dateTime: stat.dataTime),
+                style: ts.copyWith(
+                  fontSize: 20.0,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              Image.asset(
+                status.imagePath,
+                width: MediaQuery.of(context).size.width / 2,
+              ),
+              const SizedBox(height: 20.0),
+              Text(status.label,
+                  style: ts.copyWith(
                     fontSize: 40.0,
                     fontWeight: FontWeight.w700,
-                  ),),
-                  Text(
-                    DateTime.now().toString(),
-                    style: ts.copyWith(
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  Image.asset(
-                    'asset/img/mediocre.png',
-                    width: MediaQuery.of(context).size.width / 2,
-                  ),
-                  const SizedBox(height: 20.0),
-                  Text('보통', style: ts.copyWith(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.w700,
-                  ) ),
-                  const SizedBox(height: 8.0),
-                  Text('나쁘지 않네요!', style: ts.copyWith(
+                  )),
+              const SizedBox(height: 8.0),
+              Text(status.comment,
+                  style: ts.copyWith(
                     fontSize: 20.0,
                     fontWeight: FontWeight.w700,
-                  ) )
-                ],
-              ),
-            ),
-          )
-      ),
+                  ))
+            ],
+          ),
+        ),
+      )),
     );
   }
+
+  // date 객체 yyyy-mm-dd:m 형태로 변환
+  String getTimeFromDateTime({required DateTime dateTime}) {
+    return '${dateTime.year}-${dateTime.month}-${dateTime.day} ${getTimeFormat(dateTime.hour)}:${getTimeFormat(dateTime.minute)}';
+  }
+
+  String getTimeFormat(int number){
+    return number.toString().padLeft(2, '0'); // 0보가 작으면 왼쪽에 2 추가
+  }
+
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -8,6 +8,7 @@ import 'package:dusty_dust/component/main_drawer.dart';
 import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/colors.dart';
 import 'package:dusty_dust/const/data.dart';
+import 'package:dusty_dust/const/status_level.dart';
 import 'package:dusty_dust/model/stat_model.dart';
 import 'package:dusty_dust/repository/stat_repository.dart';
 import 'package:flutter/material.dart';
@@ -22,39 +23,60 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  void initState() {
-    // 생성자 함수. 위젯 생성될 때 최초 1회 실행
-    // TODO: implement initState
-    super.initState();
-    fetchData();
-  }
 
-  fetchData() async {
+  Future<List<StatModel>> fetchData() async {
     final statModels = await StatRepository.fetchData();
-    print(statModels);
+    return statModels;
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: primaryColor,
         drawer: MainDrawer(),
-        body: CustomScrollView(
-          slivers: [
-            // 플러터에서 스크롤되는 모든 것
-            MainAppBar(),
-            SliverToBoxAdapter(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  CategoryCard(),
-                  const SizedBox(
-                    height: 16.0,
+        body: FutureBuilder<List<StatModel>>(
+          future: fetchData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError){ // 에러 발생
+              return Center(
+                child: Text('에러 발생!'),
+              );
+            }
+
+            if (!snapshot.hasData){ // 데이터 로딩 중 (connectionState로 확인하면 연결할 때마다 로딩바 듬)
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            List<StatModel> stats = snapshot.data!;
+            StatModel recentStat = stats[0];
+
+            // 1 - 5, 6 - 10, 11 - 15
+            // 7이 어떤 범위에 속하는가? 최솟값 1,6,11 기준 7보다 작은 값 중 가장 큰 값 선택
+            final status = statusLevel.where((element) => element.minFineDust < recentStat.seoul).last;
+
+            return CustomScrollView(
+              slivers: [
+                // 플러터에서 스크롤되는 모든 것
+                MainAppBar(
+                  stat: recentStat,
+                  status: status,
+                ),
+                SliverToBoxAdapter(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      CategoryCard(),
+                      const SizedBox(
+                        height: 16.0,
+                      ),
+                      HourlyCard()
+                    ],
                   ),
-                  HourlyCard()
-                ],
-              ),
-            )
-          ],
+                )
+              ],
+            );
+          },
         ));
   }
 }
